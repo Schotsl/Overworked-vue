57c3c1275e14152a8c8d5b57bf518837d580643f
Sjors van Holst <sjorsvanholst@gmail.com>
2022-05-20 16:19:51 +0200

    :poop: Cleanup

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,17 +3,15 @@ module.exports = {
   env: {
     node: true
   },
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/typescript/recommended'
   ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    
     'vue/no-deprecated-slot-attribute': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
