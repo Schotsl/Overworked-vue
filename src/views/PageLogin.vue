16d718a34a07820fba6ea0a2c80fc6f3de2bbef9
Sjors van Holst <sjorsvanholst@gmail.com>
2022-05-25 18:01:46 +0200

    💄 Styled login page

--- src/views/PageLogin.vue
+++ src/views/PageLogin.vue
@@ -1,8 +1,47 @@
 <template>
   <ion-page>
-    <ion-content :fullscreen="true">
-      <ion-button @click="googleSignin">Login with Google</ion-button>
-      {{ user }}
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Login</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <div class="container">
+        <ion-text class="title"> 🥳🎉 </ion-text>
+
+        <ion-button
+          size="large"
+          shape="round"
+          class="login"
+          expand="block"
+          @click="appleSignin"
+        >
+          <ion-icon slot="start" :icon="logoApple"></ion-icon>
+          Login using Apple
+        </ion-button>
+
+        <ion-button
+          size="large"
+          shape="round"
+          class="login"
+          expand="block"
+          @click="googleSignin"
+        >
+          <ion-icon slot="start" :icon="logoGoogle"></ion-icon>
+          Login using Google
+        </ion-button>
+
+        <ion-button
+          size="large"
+          shape="round"
+          class="login"
+          expand="block"
+          @click="githubSignin"
+        >
+          <ion-icon slot="start" :icon="logoGithub"></ion-icon>
+          Login using GitHub
+        </ion-button>
+      </div>
     </ion-content>
   </ion-page>
 </template>
@@ -10,46 +49,88 @@
 <script lang="ts">
 import { initializeApp, FirebaseOptions } from "firebase/app";
 
-// TODO: Replace the following with your app's Firebase project configuration
+// TODO: This should be moved to a .env file
 const firebaseConfig: FirebaseOptions = {
+  appId: "1:863141653950:web:86813dbabb9bbec54dddef",
   apiKey: "AIzaSyB7R5pFg9qeoDVGha2mBpCMzziQ7x0rfyA",
-  authDomain: "test-49d01.firebaseapp.com",
   projectId: "test-49d01",
+  authDomain: "test-49d01.firebaseapp.com",
   storageBucket: "test-49d01.appspot.com",
   messagingSenderId: "863141653950",
-  appId: "1:863141653950:web:86813dbabb9bbec54dddef",
 };
 
 initializeApp(firebaseConfig);
 
-import { isPlatform } from "@ionic/vue";
-// import { GoogleAuth } from "@codetrix-studio/capacitor-google-auth";
-import { FirebaseAuthentication } from "@capacitor-firebase/authentication";
 import { defineComponent } from "vue";
-import { IonButton, IonPage, IonContent } from "@ionic/vue";
+import { FirebaseAuthentication } from "@capacitor-firebase/authentication";
+import { logoGoogle, logoApple, logoGithub } from "ionicons/icons";
+import {
+  IonButton,
+  IonPage,
+  IonContent,
+  IonIcon,
+  IonText,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+} from "@ionic/vue";
 
 export default defineComponent({
   name: "PageLogin",
-  data() {
-    return {
-      user: "",
-    };
-  },
+
   components: {
+    IonText,
+    IonIcon,
     IonPage,
+    IonTitle,
+    IonHeader,
     IonButton,
+    IonToolbar,
     IonContent,
   },
+
   methods: {
-    async googleSignin() {
+    async appleSignin() {
       const result = await FirebaseAuthentication.signInWithApple();
-      this.user = JSON.stringify(result.user);
+      console.log(result);
+    },
+    async googleSignin() {
+      const result = await FirebaseAuthentication.signInWithGoogle();
+      console.log(result);
+    },
+    async githubSignin() {
+      const result = await FirebaseAuthentication.signInWithGithub();
+      console.log(result);
     },
   },
-  computed: {
-    isWeb() {
-      return !isPlatform("ios") && !isPlatform("android");
-    },
+
+  setup() {
+    return {
+      logoApple,
+      logoGoogle,
+      logoGithub,
+    };
   },
 });
 </script>
+
+<style>
+.title {
+  margin: 1.5rem 1.5rem 2rem;
+  font-size: 5rem;
+}
+
+.login {
+  width: 100%;
+  margin: 0.75rem;
+  font-size: 1.15rem;
+}
+
+.container {
+  margin: 2rem 2.5rem 2.5rem;
+  display: flex;
+
+  align-items: center;
+  flex-direction: column;
+}
+</style>
