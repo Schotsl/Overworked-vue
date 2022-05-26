f378fe72fc001dbe303080724a195f7724cf1bf0
Wouter de Bruijn <wouter@hedium.nl>
2022-05-26 15:26:33 +0200

    :wrench: Allowed non-null-assertion

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,6 @@ module.exports = {
 
     "vue/no-deprecated-slot-attribute": "off",
     "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
   },
 };
