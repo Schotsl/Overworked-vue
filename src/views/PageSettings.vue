e4f5abe0512fe6e6ddc54963fe4d366686b0cc76
Wouter de Bruijn <wouter@hedium.nl>
2022-05-26 16:16:49 +0200

    :sparkles: Added Logout

--- src/views/PageSettings.vue
+++ src/views/PageSettings.vue
@@ -12,6 +12,12 @@
           <ion-title size="large">Settings</ion-title>
         </ion-toolbar>
       </ion-header>
+      <ion-item>
+        <ion-label>{{ fullName }}</ion-label>
+        <ion-button slot="end" fill="clear" size="small" @click="logout">
+          Logout
+        </ion-button>
+      </ion-item>
     </ion-content>
   </ion-page>
 </template>
@@ -24,7 +30,13 @@ import {
   IonHeader,
   IonToolbar,
   IonContent,
+  IonItem,
+  IonButton,
+  IonLabel,
 } from "@ionic/vue";
+
+import store from "@/store";
+import router from "@/router";
 
 export default defineComponent({
   name: "PageSettings",
@@ -32,8 +44,23 @@ export default defineComponent({
     IonPage,
     IonTitle,
     IonHeader,
+    IonLabel,
     IonToolbar,
     IonContent,
+    IonItem,
+    IonButton,
+  },
+  methods: {
+    logout() {
+      console.log("Logging out...");
+      store.dispatch.authentication.LOGOUT();
+      router.push("/");
+    },
+  },
+  computed: {
+    fullName() {
+      return `${store.state.authentication.user?.first} ${store.state.authentication.user?.last}`;
+    },
   },
 });
 </script>
