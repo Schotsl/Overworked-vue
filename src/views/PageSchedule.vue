eb2b2f4db3b0b423cf0f0606b69f06d0d4b2491e
Wouter de Bruijn <wouter@hedium.nl>
2022-05-27 14:31:24 +0200

    :fire: Removed old layout

--- src/views/PageSchedule.vue
+++ src/views/PageSchedule.vue
@@ -12,42 +12,6 @@
           <ion-title size="large">Schedule</ion-title>
         </ion-toolbar>
       </ion-header>
-
-      <ion-list>
-        <ion-radio-group value="biff">
-          <ion-list-header>
-            <ion-label>Location</ion-label>
-          </ion-list-header>
-
-          <ion-item>
-            <ion-label>Basic-Fit Toolenburg</ion-label>
-            <ion-radio slot="start" value="biff"></ion-radio>
-          </ion-item>
-
-          <ion-item>
-            <ion-label>Basic-Fit Nieuw-Vennep</ion-label>
-            <ion-radio slot="start" value="griff"></ion-radio>
-          </ion-item>
-
-          <ion-item>
-            <ion-label>Basic-Fit J.C. Beetslaan</ion-label>
-            <ion-radio slot="start" value="buford"></ion-radio>
-          </ion-item>
-        </ion-radio-group>
-      </ion-list>
-
-      <ion-item>
-        <ion-label>Day</ion-label>
-        <ion-select value="0" ok-text="Okay" cancel-text="Dismiss">
-          <ion-select-option value="0">Monday</ion-select-option>
-          <ion-select-option value="1">Tuesday</ion-select-option>
-          <ion-select-option value="2">Wednesday</ion-select-option>
-          <ion-select-option value="3">Thursday</ion-select-option>
-          <ion-select-option value="4">Friday</ion-select-option>
-          <ion-select-option value="5">Saturday</ion-select-option>
-          <ion-select-option value="6">Sunday</ion-select-option>
-        </ion-select>
-      </ion-item>
     </ion-content>
   </ion-page>
 </template>
@@ -56,17 +20,9 @@
 import { defineComponent } from "vue";
 import {
   IonPage,
-  IonList,
-  IonItem,
   IonTitle,
-  IonLabel,
-  IonRadio,
   IonHeader,
-  IonSelect,
   IonToolbar,
-  IonRadioGroup,
-  IonListHeader,
-  IonSelectOption,
   IonContent,
 } from "@ionic/vue";
 
@@ -74,17 +30,9 @@ export default defineComponent({
   name: "PageSchedule",
   components: {
     IonPage,
-    IonList,
-    IonItem,
     IonTitle,
-    IonLabel,
-    IonRadio,
     IonHeader,
-    IonSelect,
     IonToolbar,
-    IonRadioGroup,
-    IonListHeader,
-    IonSelectOption,
     IonContent,
   },
 });
