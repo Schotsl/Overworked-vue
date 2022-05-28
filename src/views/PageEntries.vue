e5896123824973ece53c4f84ea1157532578ec69
Wouter de Bruijn <wouter@hedium.nl>
2022-05-28 15:45:00 +0200

    :construction: Added entry list

--- src/views/PageEntries.vue
+++ src/views/PageEntries.vue
@@ -10,6 +10,19 @@
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">Entries</ion-title>
+          <template>
+            <ion-list>
+              <ion-item v-for="entry in sessionEntries" :key="entry.uuid">
+                <ion-label>
+                  <h2>{{ entry.person }}</h2>
+                  <p>{{ entry.machine }}</p>
+                </ion-label>
+                <ion-button slot="end" fill="clear" @click="deleteEntry(entry)">
+                  <ion-icon name="trash"></ion-icon>
+                </ion-button>
+              </ion-item>
+            </ion-list>
+          </template>
         </ion-toolbar>
       </ion-header>
     </ion-content>
@@ -25,6 +38,8 @@ import {
   IonToolbar,
   IonContent,
 } from "@ionic/vue";
+import store from "@/store";
+import { Entry } from "@/store/types";
 
 export default defineComponent({
   name: "PageEntries",
@@ -34,6 +49,27 @@ export default defineComponent({
     IonHeader,
     IonToolbar,
     IonContent,
+  },
+  data() {
+    return {
+      loading: true,
+    };
+  },
+  computed: {
+    sessionEntries() {
+      return store.state.userdata.session?.entries ?? [];
+    },
+  },
+  methods: {
+    async deleteEntry(entry: Entry) {
+      console.log("Not Impemented", entry);
+      // await store.dispatch("deleteEntry", entry);
+    },
+  },
+  async mounted() {
+    this.loading = true;
+    await store.dispatch.userdata.FETCH_SESSION_ENTRIES();
+    this.loading = false;
   },
 });
 </script>
